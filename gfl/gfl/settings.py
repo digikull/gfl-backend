9e6b5d6de11c6ec02dbfb1ca4db75c3ec8d8790e
Amey1109 <bhogaonkaramey@gmail.com>
2021-04-19 19:49:03 +0530

    Switched to RDS

--- gfl/gfl/settings.py
+++ gfl/gfl/settings.py
@@ -104,21 +104,21 @@ WSGI_APPLICATION = 'gfl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {'default':
-#              {'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#               'NAME': 'gfl',
-#               'USER': 'postgres',
-#               'PASSWORD': 'gfloctbatch',
-#               'HOST': 'database-1.cos17dl5jv9r.ap-south-1.rds.amazonaws.com',
-#               'PORT': 5432, }
-#              }
+DATABASES = {'default':
+             {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+              'NAME': 'gfl',
+              'USER': 'postgres',
+              'PASSWORD': 'gfloctbatch',
+              'HOST': 'database-1.cos17dl5jv9r.ap-south-1.rds.amazonaws.com',
+              'PORT': 5432, }
+             }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
